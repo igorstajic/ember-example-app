0ea007df609b35fe5419cc453f7dbe673acb3937
istajic <igorstajic273@gmail.com>
2017-03-22 11:17:54 +0100

    removed unused serializer

--- mirage/serializers/user.js
+++
@@ -1,8 +0,0 @@
-import BaseSerializer from './application';
-
-export default BaseSerializer.extend({
-  // serialize(object, request) {
-  //   let json = BaseSerializer.prototype.serialize.apply(this, arguments);
-  //   return json;
-  // }
-});
